bd7dcbc30429208a5e97b4d0ccbaa864c5fd48bb
Dinadi80 <63351109+Dinadi80@users.noreply.github.com>
2020-06-25 23:05:41 +1000

    Delete test.js

--- javascript/test.js
+++
@@ -1,6 +0,0 @@
-﻿let userName = "Jack";
-let userWeight = 87
-
-let weightDescription;
-weightDescription = userWeight > 90 ? "redundant" : "normal";
-console.log(userName + "has" + weightDescription + "weight");
