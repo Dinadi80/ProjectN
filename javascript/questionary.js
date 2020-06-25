2e3a3a763135557edf1799424b2e43e8b10cf86c
Dinadi80 <63351109+Dinadi80@users.noreply.github.com>
2020-06-25 23:08:29 +1000

    Delete questionary.js

--- javascript/questionary.js
+++
@@ -1,6 +0,0 @@
-﻿
-let firstName = prompt("Введите имя");
-let soName = prompt("Введите фамилию");
-let age = +prompt("Введите возраст");
-console.log("Пользователь: " + firstName + " " + soName );
-console.log("Возраст: " + age); 
