d932a69b688f6beb4e40545bb7ae86952ef20cc4
Dinadi80 <63351109+Dinadi80@users.noreply.github.com>
2020-06-25 23:03:30 +1000

    Delete waight.js

--- javascript/waight.js
+++
@@ -1,6 +0,0 @@
-﻿
-let waight = + prompt("Введите ваш вес");
-let height = + prompt("Введите ваш рост");
-let coefficient = waight / Math.pow(height, 2);  
-let  doYouhaveExesweight = coefficient > 28;
-alert("У вас есть лишний вес: " + doYouhaveexeswaight);
